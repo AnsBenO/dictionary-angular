0df6e9eaa5c42e5a26ca924b204fdc187c33c253
AnsBenO <ans.benomar@gmail.com>
2024-01-21 23:41:14 +0100

    Added about page content

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AboutComponent } from "./about/about.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
     declarations: [AboutComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, FontAwesomeModule],
 })
 export class AboutModule {}
--- src/app/pages/about/about/about.component.ts
+++ src/app/pages/about/about/about.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: "app-about",
+    templateUrl: "./about.component.html",
+    styleUrls: ["./about.component.css"],
 })
 export class AboutComponent {
-
+    linkedIn = faLinkedin as IconProp;
+    gitHub = faGithub as IconProp;
 }
